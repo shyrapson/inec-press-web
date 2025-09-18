9f2c66625e9d53a65a2db3490f8e1bbf6abc0deb
Rabiu Saidu <58531876+shyrapson@users.noreply.github.com>
2025-09-18 14:18:03 +0100

    fix: quick fix

--- src/components/organisms/home/hero-section.tsx
+++ src/components/organisms/home/hero-section.tsx
@@ -8,9 +8,7 @@ const HeroSection = () => {
         <div className="relative z-10 flex items-center justify-center min-h-[calc(100vh-80px)] px-4 py-8">
           <div className="text-center text-white md:max-w-5xl mx-auto">
             <div className="text-2xl sm:text-3xl md:text-[46px] mb-6 md:mb-8 font-bold text-balance leading-tight">
-              <h1 className="mb-2 md:mb-3">
-                INEC PORTAL FOR THE ENGAGEMENT OF
-              </h1>
+              <h1 className="mb-3">INEC PORTAL FOR THE RECRUITMENT OF</h1>
               <h1 className="">ELECTION STAFF</h1>
             </div>
             <p className="text-sm md:text-lg text-gray-200 max-w-2xl mx-auto leading-relaxed">
